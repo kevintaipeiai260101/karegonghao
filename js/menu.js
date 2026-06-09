a2082a0ada6a68c59601fa759208879e93320192
kevintaipeiai260101 <kevintaipeiai260101@gmail.com>
2026-06-09 16:19:32 +0800

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
             <li><a href="story.html">品牌故事</a></li>
             <li><a href="system.html">中轉系統</a></li>
             <li><a href="community.html">社區共好</a></li>
+            <li><a href="partners.html">共好夥伴</a></li>
             <li><a href="order.html">訂餐</a></li>
             <li><a href="faq.html">FAQ</a></li>
             <li><a href="contact.html">聯絡我們</a></li>
